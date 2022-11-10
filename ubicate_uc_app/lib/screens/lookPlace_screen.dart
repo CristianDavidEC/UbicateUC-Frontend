40b37dea64aa2f7065c66028a6bdfd4ae7eb9b1b
juanesosocasta <86965058+juanesosocasta@users.noreply.github.com>
2022-11-10 15:36:57 -0500

    creacion pantalla de busqueda

--- ubicate_uc_app/lib/screens/lookPlace_screen.dart
+++ ubicate_uc_app/lib/screens/lookPlace_screen.dart
@@ -1,22 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:front_ubicate_uc/widgets/widgets.dart';
 
 class lookPlaceScreen extends StatelessWidget {
   const lookPlaceScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Consultar sitio'),
+        title: const Text('Search'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: MySearchDelegate(),
+              );
+            },
+          )
+        ],
       ),
-      body: Center(
-        child: Text('Ventana para consultar sitio'),
-      )
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: setMessage,
-      //   child: const Icon(Icons.add)
-      //   ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+ class MySearchDelegate extends SearchDelegate {
+  @override
+  Widget? buildLeading(BuildContext context) => IconButton(
+        icon: const Icon(Icons.arrow_back),
+        onPressed: () => close(context, null), //cerrar barra de busqueda,
+      );
+  @override
+  List<Widget>? buildActions(BuildContext context) => [
+        IconButton(
+          icon: const Icon(Icons.clear),
+          onPressed: () {
+            if (query.isEmpty) {
+              close(context, null);
+            } else {
+              query = '';
+            }
+          },
+        ),
+      ];
+  @override
+  Widget buildResults(BuildContext context) => Center(
+        child: Text(
+          query,
+          style: const TextStyle(fontSize: 64),
+        ),
+      );
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestions = [
+      'hola',
+      'vaca',
+      'starWars',
+      'juan',
+    ];
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: ((context, index) {
+        final suggestion = suggestions[index];
+        return ListTile(
+          title: Text(suggestion),
+          onTap: () {
+            query = suggestion;
+          },
+        );
+      }),
     );
   }
 }
